0da00bf7efb2474ce972e5aa830d2c494d8ebccb
Michal Badura <badura.mike@gmail.com>
2019-07-07 20:18:03 +0200

    Refactor twelve-days with IntStream.

--- twelve-days/src/main/java/TwelveDays.java
+++ twelve-days/src/main/java/TwelveDays.java
@@ -1,45 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class TwelveDays {
-    private static final List<String> daysList = new ArrayList<>(
-            List.of("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
-                    "eleventh", "twelfth")
-    );
+    private static final String STARTING_VERSE = "On the %s day of Christmas my true love gave to me: %s.\n";
 
-    private static final List<String> giftsList = new ArrayList<>(
-            List.of("a Partridge in a Pear Tree", "two Turtle Doves, ", "three French Hens, ",
-                    "four Calling Birds, ", "five Gold Rings, ", "six Geese-a-Laying, ", "seven Swans-a-Swimming, ",
-                    "eight Maids-a-Milking, ", "nine Ladies Dancing, ", "ten Lords-a-Leaping, ",
-                    "eleven Pipers Piping, ", "twelve Drummers Drumming, ")
-    );
+    private static final String[] ORDINALS = {"first", "second", "third", "fourth", "fifth",
+            "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"};
 
-    String verse(int verseNumber) {
-        StringBuilder sB = new StringBuilder();
-        int indexNumber = verseNumber - 1;
-        sB.append("On the ")
-                .append(daysList.get(indexNumber))
-                .append(" day of Christmas my true love gave to me: ");
+    private static final String[] GIFTS = {"a Partridge in a Pear Tree", "two Turtle Doves",
+            "three French Hens", "four Calling Birds", "five Gold Rings", "six Geese-a-Laying",
+            "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing", "ten Lords-a-Leaping",
+            "eleven Pipers Piping", "twelve Drummers Drumming"};
 
-        for (int i = indexNumber; i >= 0; i--) {
-            if (verseNumber > 1 && i == 0) {
-                sB.append("and ");
-            }
-            sB.append(giftsList.get(i));
+    String verse(int verseNumb) {
+        return String.format(STARTING_VERSE, ORDINALS[verseNumb - 1],
+                formatGifts(verseNumb));
+    }
+
+    String formatGifts(int verseNumb) {
+        if (verseNumb == 1) {
+            return GIFTS[0];
+        } else {
+            return IntStream
+                    .iterate(verseNumb - 1, i -> i - 1)
+                    .limit(verseNumb - 1)
+                    .mapToObj(i -> GIFTS[i])
+                    .collect(Collectors.joining(", "))
+                    + ", and "
+                    + GIFTS[0];
         }
-        sB.append(".\n");
-        return sB.toString();
     }
 
     String verses(int startVerse, int endVerse) {
-        StringBuilder sB = new StringBuilder();
-        for (int i = startVerse; i <= endVerse; i++) {
-            sB.append(verse(i));
-            if (i < endVerse) {
-                sB.append("\n");
-            }
-        }
-        return sB.toString();
+        return IntStream
+                .rangeClosed(startVerse, endVerse)
+                .mapToObj(i -> verse(i))
+                .collect(Collectors.joining("\n"));
     }
 
     String sing() {
