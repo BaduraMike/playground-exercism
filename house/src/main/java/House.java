ffce8f289d0cc20659b8adb63a167cafd5c9233e
Michal Badura <badura.mike@gmail.com>
2019-07-10 21:22:56 +0200

    Refactor House.

--- house/src/main/java/House.java
+++ house/src/main/java/House.java
@@ -1,27 +1,26 @@
 public class House {
-    private static String[] nounsArray = {"house that Jack built", "malt", "rat", "cat", "dog", "cow with the crumpled horn",
-            "maiden all forlorn", "man all tattered and torn", "priest all shaven and shorn",
-            "rooster that crowed in the morn", "farmer sowing his corn", "horse and the hound and the horn"};
-
-    private static String[] verbsArray = {"lay in", "ate", "killed", "worried", "tossed",
-            "milked", "kissed", "married", "woke", "kept", "belonged to"};
+    private static final String[][] STRINGS = new String[][]{
+            {"house that Jack built", null},
+            {"malt", "lay in"},
+            {"rat", "ate"},
+            {"cat", "killed"},
+            {"dog", "worried"},
+            {"cow with the crumpled horn", "tossed"},
+            {"maiden all forlorn", "milked"},
+            {"man all tattered and torn", "kissed"},
+            {"priest all shaven and shorn", "married"},
+            {"rooster that crowed in the morn", "woke"},
+            {"farmer sowing his corn", "kept"},
+            {"horse and the hound and the horn", "belonged to"}
+    };
 
     public String verse(int verseNumber) {
+        StringBuilder sB = new StringBuilder("This is the ");
 
-        StringBuilder sB = new StringBuilder();
-
-        for (int i = verseNumber - 1; i >= 0; i--) {
-
-            if (i == verseNumber - 1) {
-                sB.append("This is the ");
-            } else {
-                sB.append(" that ").append(verbsArray[i]).append(" the ");
-            }
-            sB.append(nounsArray[i]);
-            if (i == 0) {
-                sB.append('.');
-            }
+        for (int i = verseNumber - 1; i > 0; i--) {
+            sB.append(String.format("%s that %s the ", STRINGS[i][0], STRINGS[i][1]));
         }
+        sB.append(String.format("%s.", STRINGS[0][0]));
 
         return sB.toString();
     }
@@ -29,13 +28,11 @@ public class House {
     public String verses(int startVerse, int endVerse) {
         StringBuilder sB = new StringBuilder();
 
-        for (int i = startVerse; i <= endVerse; i++) {
-            sB.append(verse(i));
-
-            if (i < endVerse) {
-                sB.append('\n');
-            }
+        for (int i = startVerse; i < endVerse; i++) {
+            sB.append(String.format("%s\n", verse(i)));
         }
+        sB.append(verse(endVerse));
+
         return sB.toString();
     }
 
